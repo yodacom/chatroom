d156883ba4654870f58402e50f37c00ff039c082
Jeremy Black <jb@yodacom.com>
2017-05-17 05:13:12 -0600

    added socket.io module and wrapped Express in http.Server

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-
-
+var socket_io = require("socket.io");
+var http = require("http");
 var express = require("express");
 
 var app = express();
 app.use(express.static("public"));
-app.listen(process.env.PORT || 8080);
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+io.on("connection", function(socket){
+    console.log("Client connected");
+}):
+
+server.listen(process.env.PORT || 8080);
